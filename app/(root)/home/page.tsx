2f33929bead404e8f7b723e3d58dead8733c9778
joel kalema <joelkalema63@gmail.com>
2024-05-16 03:40:22 +0200

    add animations

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,7 +1,12 @@
-import { Prompt } from "next/font/google";
-import TextAbout from "./_componets/text";
-import Timer from "@/components/count/timer"
+"use client"
 
+import { gsap } from "gsap";
+import TextAbout from "./_componets/text";
+import Timer from "@/components/count/timer";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+import { Prompt } from "next/font/google";
 
 const prompt = Prompt({
   subsets: ['latin', 'latin-ext', 'thai', 'vietnamese'],
@@ -9,18 +14,33 @@ const prompt = Prompt({
 })
 
 export default function HomePage() {
+  const menuItemRef = useRef<HTMLDivElement>(null);
+  useGSAP(() => {
+    if (menuItemRef.current) {
+      const tl = gsap.timeline();
+      tl.from(menuItemRef.current.children, {
+        y: 100,
+        opacity: 0,
+        duration: 2,
+        delay: 0.5,
+        stagger: {
+          amount: 0.7,
+        }
+      });
+    }
+  });
+
   return (
     <div className="flex justify-center items-center px-32 home">
-        <div>
-            <div className="mt-[10rem] text-center">
-                <h1 className={`${prompt.className} text-6xl font-extrabold`}>TABLE OF EAGLES</h1>
-                <h2 className="mt-6 ">Nov 29 - Dec 01</h2>
-                <Timer />
-            </div>
-
-            <TextAbout />
+      <div ref={menuItemRef}>
+        <div className="mt-[10rem] text-center">
+          <h1 className={`${prompt.className} font-bold text-6xl`}>TABLE OF EAGLES</h1>
+          <h2 className="mt-6 ">Nov 29 - Dec 01</h2>
+          <Timer />
         </div>
-      
+
+        <TextAbout />
+      </div>
     </div>
   );
 }
