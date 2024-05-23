6099ad5eb86a7e3ec6be1013fa5848188a40e0d3
joel kalema <joelkalema63@gmail.com>
2024-05-23 11:40:27 +0200

    update animation

--- app/(root)/home/_componets/text.jsx
+++ app/(root)/home/_componets/text.jsx
@@ -25,9 +25,9 @@ export default function TextAbout() {
           });
           
           tl.from(text.current.children, {
-            x: -200,
+            x: -250,
             opacity: 0,
-            duration: 1.5,
+            duration: 2,
             delay: 0.5,
             stagger: {
               amount: 0.7,
@@ -37,7 +37,7 @@ export default function TextAbout() {
 
     return (
         <div id="about">
-            <div className="w-4/6 float-right overflow-hidden mt-[10rem] relative text-[1.1rem] mr-20" ref={text}>
+            <div className="w-4/6 pl-10 float-right overflow-hidden mt-[10rem] relative text-[1.1rem] mr-10" ref={text}>
                 <h1 className={`${time.className} font-extrabold text-3xl mb-10 text-[#d3b975]`}>Welcome to the Table Of Eagles</h1>
                 <p><span className="text-[#d3b975] font-extrabold">TABLE OF EAGLES</span> is a conference carrying a vision of the Preparation of the Bride of Christ for the soon return of our Lord Jesus (The Rapture). It seeks to build a Prophetic Nation of believers through a conference once every year focusing on:</p>
                 <ul className="list-disc ml-6 mt-4">
--- app/(root)/tentative-chedule/page.jsx
+++ app/(root)/tentative-chedule/page.jsx
@@ -37,6 +37,7 @@ export default function TentativeSchedule() {
     }, []);
 
     const menuItemRef = useRef(null);
+    const friday = useRef(null);
     const saturday = useRef(null);
     const sunday = useRef(null)
 
@@ -89,42 +90,63 @@ export default function TentativeSchedule() {
                 amount: 0.7,
             },
         });
+
+        const scrool3 = gsap.timeline({
+            scrollTrigger: {
+                trigger: friday.current,
+                start: "top-=200px center",
+                end: "bottom bottom-=200px",
+                scrub: true,
+            }
+        });
+
+        scrool3.from(friday.current.children, {
+            x: 200,
+            opacity: 0,
+            duration: 1.5,
+            delay: 0.5,
+            stagger: {
+                amount: 0.7,
+            },
+        });
     });
 
     return (
         <main className="">
             <Nav />
-            <div className="w-4/5 mx-auto pt-[15rem] text-xl" ref={menuItemRef}>
-                <h1 data-scroll data-scroll-speed='0.8' className={`${title.className} uppercase text-4xl mb-10`}>Tentative Schedule</h1>
-                <div className="border-t-[1px] border-[#9b9a96] py-10" data-scroll data-scroll-speed='0.6'>
+            <div className="w-4/5 mx-auto pt-[15rem] px-20 text-xl" ref={menuItemRef}>
+                <h1 data-scroll data-scroll-speed='0.8' className={`${title.className} uppercase text-5xl my-20 text-[#d3b975]`}>Tentative Schedule</h1>
+                <p data-scroll data-scroll-speed='0.7' className={`${subTitle.className} uppercase text-3xl mb-2 text-[#d3b975]`}>Nov 29 - Dec 01</p>
+                <p data-scroll data-scroll-speed='0.6' className={`${subTitle.className} uppercase text-2xl mb-20 w-3/4`}>3 days of the prophetic, of impartation, of the power of God, of size wonders, and miracles, and three days of Salvation you cannot miss.</p>
+                
+                <div ref={friday} className="pt-10" data-scroll data-scroll-speed='0.5'>
                     <h2 className={`${subTitle.className} uppercase text-6xl mb-6`}>Friday:</h2>
                     <p>5:00pm-6:00pm (Opening prayer & Worship VO1)</p>
                     <p>6:00pm-7:30pm (Apostle Enmith)</p>
                     <p>7:30pm-8:00pm (Worship with TFC choir)</p>
                     <p>8:00pm-9:30pm (Apostle Tomi Arayomi)</p>
                     <p>9:30pm-10:30pm (Consecration & Closing)</p>
-                    <p></p>
                 </div>
 
-                <div ref={saturday} className="border-t-[1px] border-[#9b9a96] py-10 overflow-hidden" data-scroll data-scroll-speed='0.2'>
-                    <h2 className={`${subTitle.className} uppercase text-6xl`}>Saturday:</h2>
+                <div ref={saturday} className="pt-10 overflow-hidden" data-scroll data-scroll-speed='0.2'>
+                    <h2 className={`${subTitle.className} uppercase text-6xl mb-6`}>Saturday:</h2>
 
 
-                    <h3>Morning Glory</h3>
+                    <h3 className="flex items-center text-[#d3b975] mb-4 text-2xl"><CiPaperplane /> Morning Glory</h3>
                     <p>9:00am-10:00am (Intercession)</p>
                     <p>10:00am-10:50am (Pastor Daniel Sandoval)</p>
                     <p>10:50 am-11:00 am (Offering & Closing)</p>
 
 
 
-                    <h3>Second Service</h3>
+                    <h3 className="flex items-center text-[#d3b975] mb-4 text-2xl mt-10"><CiPaperplane />Second Service</h3>
                     <p>3:00 pm-3:40 pm (Worship with TFC Choir)</p>
                     <p>3:40 pm-4:45 pm (Pastor Kofi Dartey)</p>
                     <p>4:45pm-4:55pm (Offering & Closing)</p>
 
 
 
-                    <h3>Evening Service</h3>
+                    <h3 className="flex items-center text-[#d3b975] mb-4 text-2xl mt-10"><CiPaperplane />Evening Service</h3>
                     <p>7:00pm-7:30pm (Opening Prayer)</p>
                     <p>7:30pm-8:15pm (Worship with Pastor Eddy James)</p>
                     <p>8:15pm-9:45pm (Apostle Randolph Adjei)</p>
@@ -132,8 +154,8 @@ export default function TentativeSchedule() {
 
                 </div>
 
-                <div ref={sunday} className="border-t-[1px] border-[#9b9a96] py-10 pb-[20rem] overflow-hidden" data-scroll data-scroll-speed='0.1'>
-                    <h2 className={`${subTitle.className} uppercase text-6xl`}>Sunday: Miracles Services</h2>
+                <div ref={sunday} className="py-10 pb-[20rem] overflow-hidden" data-scroll data-scroll-speed='0.1'>
+                    <h2 className={`${subTitle.className} uppercase text-6xl`}>Sunday:<br /> Miracles Services</h2>
                     <p>5:00pm-5:30pm (Opening Prayer)</p>
                     <p>5:30pm-6:15pm (Worship with Pastor Eddy James)</p>
                     <p>6:15pm-8:45pm (Prophet Christopher Beleke Preaching, Service, Offering)</p>
