8837e371bb0cf6478c583fa48bbd5d7343a3a526
joel kalema <joelkalema63@gmail.com>
2024-05-23 23:58:32 +0200

    updeted links

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -22,14 +22,14 @@ export default function Nav() {
         <img src="/logo.png" alt="" className="" />
       </div>
 
-      <div className="w-4/6 flex justify-between items-center">
+      <div className="w-3/6 flex justify-between items-center">
         <div className="flex w-4/6 justify-between">
-         <ScrollLink className="cursor-pointer a" to="home"><Link href="/">HOME</Link></ScrollLink>
-         <ScrollLink className="cursor-pointer a" to="about">ABOUT</ScrollLink>
-          <ScrollLink className="cursor-pointer a" to="speaker">SPEAKERS</ScrollLink>
+         <ScrollLink className="cursor-pointer a" to="#home"><Link href="/">HOME</Link></ScrollLink>
+         <ScrollLink className="cursor-pointer a" to="#about">ABOUT</ScrollLink>
+          <ScrollLink className="cursor-pointer a" to="#speaker">SPEAKERS</ScrollLink>
           {/* <li className="cursor-pointer a">INFOS</li>
           <li className="cursor-pointer a">FAQS</li> */}
-          <Link className="uppercase cursor-pointer a" href="/tentative-chedule">Tentative Schedule</Link>
+          <Link className="uppercase cursor-pointer a" href="/tentative-chedule">PROGRAM</Link>
         </div>
         <Link href="" className="box">
           <p>REGISTER NOW</p>
