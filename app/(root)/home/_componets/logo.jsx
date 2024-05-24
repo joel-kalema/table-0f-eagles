5c3819c0a273418a5607e3bc8768db082a61fca1
joel kalema <joelkalema63@gmail.com>
2024-05-24 18:01:27 +0200

    update the logo magins

--- app/(root)/home/_componets/logo.jsx
+++ app/(root)/home/_componets/logo.jsx
@@ -15,7 +15,7 @@ export default function People() {
     const [isLoading, setLoading] = useState(true);
     return (
         <div className="logo flex justify-center items-center flex-col">
-            <div className="md:w-1/43 lg:w-1/4 overflow-hidden mb-10">
+            <div className="w-2/4 lg:w-1/4 overflow-hidden mb-10">
                 <Image src="/logo.png" alt="about" loading="lazy" layout="responsive" width={500} height={500} quality={80}
                     className={`
                     duration-700 ease-in-out group-hover:opacity-75
@@ -27,7 +27,7 @@ export default function People() {
                 />
 
             </div>
-            <p className={`${time.className} w-4/6 text-center uppercase mb-10`}>We believe in the apostolic end-time mandate of the Church. At our conference, we delve into the spiritual and marketplace realms, equipping believers to fulfill their divine purpose. Armed with a strong understanding of this mandate, we march forward, impacting nations with the love and power of God.</p>
+            <p className={`${time.className} w-5/6 text-sm md:text-xl lg:w-4/6 text-center uppercase mb-10`}>We believe in the apostolic end-time mandate of the Church. At our conference, we delve into the spiritual and marketplace realms, equipping believers to fulfill their divine purpose. Armed with a strong understanding of this mandate, we march forward, impacting nations with the love and power of God.</p>
             <Link href="" className="box">
                 <p>REGISTER NOW</p>
             </Link>
