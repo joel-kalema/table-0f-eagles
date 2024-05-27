a98e80b166d158e354580cbb42c1466edcf01a61
joel kalema <joelkalema63@gmail.com>
2024-05-27 11:06:40 +0200

    update media query of the home page

--- app/(root)/tentative-chedule/page.jsx
+++ app/(root)/tentative-chedule/page.jsx
@@ -116,11 +116,11 @@ export default function TentativeSchedule() {
         <main className="">
             <Navbar />
             <div className="lg:w-4/5 mx-auto pt-[10rem] lg:pt-[15rem] px-4 lg:px-20 text-xl" ref={menuItemRef}>
-                <h1 data-scroll data-scroll-speed='0.6' className={`${title.className} uppercase text-3xl lg:text-5xl my-6 lg:my-20 text-[#d3b975]`}>Tentative Schedule</h1>
+                <h1 data-scroll data-scroll-speed='0.6' className={`${title.className} uppercase text-3xl lg:text-5xl my-10 lg:my-20 text-[#d3b975]`}>Tentative Schedule</h1>
                 <p data-scroll data-scroll-speed='0.5' className={`${subTitle.className} uppercase text-xl md:text-3xl mb-2 text-[#d3b975]`}>Nov 29 - Dec 01</p>
                 <p data-scroll data-scroll-speed='0.4' className={`${subTitle.className} uppercase text-xl md:text-3xl mb-20 lg:w-3/4`}>3 days of the prophetic, of impartation, of the power of God, of size wonders, and miracles, and three days of Salvation you cannot miss.</p>
 
-                <div ref={friday} className="pt-10 text-sm lg:text-xl" data-scroll data-scroll-speed='0.3'>
+                <div ref={friday} className="pt-10 text-sm lg:text-xl overflow-hidden" data-scroll data-scroll-speed='0.3'>
                     <h2 className={`${subTitle.className} uppercase text-3xl lg:text-6xl mb-4`}>Friday:</h2>
                     <p>5:00pm-5:15 Prayer</p>
                     <p>5:15-6:00pm Worship VO1</p>
