0dbc3d93a8b3ec8daff9e63e4e4084755629aac4
joel kalema <joelkalema63@gmail.com>
2024-06-02 01:27:59 +0200

    update margin

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 "use client"
 
 import { useEffect } from "react";
+
+import dynamic from "next/dynamic";
+
 import Nav from "@/components/Navbar/navbar";
 import HomePage from "./(root)/home/page";
-import Speakers from "./(root)/home/_componets/speakers/page";
-import Location from "./(root)/home/_componets/map";
-import People from "./(root)/home/_componets/logo";
-import VideoPresention from "./(root)/home/_componets/video";
+// import Speakers from "./(root)/home/_componets/speakers/page";
+// import Location from "./(root)/home/_componets/map";
+// import People from "./(root)/home/_componets/logo";
+// import VideoPresention from "./(root)/home/_componets/video";
 import Footer from "@/components/footer/footer";
+
+const DynamicVideoPresention = dynamic(() => import("./(root)/home/_componets/video"))
+const DynamicSpeakers = dynamic(() => import("./(root)/home/_componets/speakers/page"))
+const DynamicLocation = dynamic(() => import("./(root)/home/_componets/map"))
+const DynamicPeople = dynamic(() => import("./(root)/home/_componets/logo"))
 
 export default function Home() {
   useEffect(() => {
@@ -27,10 +35,10 @@ export default function Home() {
     <main className="">
       <Nav />
       <HomePage />
-      <VideoPresention />
-      <Speakers />
-      <Location />
-      <People />
+      <DynamicVideoPresention />
+      <DynamicSpeakers />
+      <DynamicLocation />
+      <DynamicPeople />
       <Footer />
     </main>
   );
